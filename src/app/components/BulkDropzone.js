e114ed93e71ee548b3be2e367e1a3d885ddae2da
avokeys <alexander.ayala.palacin@gmail.com>
2023-01-24 01:50:24 +0000

    start upload button receives accepted files data

--- src/app/components/BulkDropzone.js
+++ src/app/components/BulkDropzone.js
@@ -1,50 +1,71 @@
-import React, {useMemo} from 'react';
-import {useDropzone} from 'react-dropzone';
+import { Button } from '@material-ui/core';
+import { size } from 'lodash';
+import React, { useMemo,useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useDispatch } from 'react-redux';
 
 
 
 const activeStyle = {
-    borderColor: '#2196f3',
+  borderColor: '#2196f3',
+};
+
+const acceptStyle = {
+  borderColor: '#00e676',
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744',
+};
+
+export function BulkDropzone(props)
+{
+  const baseStyle = {
+    flex: 1,
+    fontSize: props.fontSize,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: props.padding,
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out',
   };
   
-  const acceptStyle = {
-    borderColor: '#00e676',
-  };
+
+  const dispatch = useDispatch();
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+    acceptedFiles,
+  } = useDropzone();
+
+  const acceptedFileItems = acceptedFiles.map((file) => {
+
+// {console.log(acceptedFiles,"accedp file")}
+{props.updateAcceptedFileData(acceptedFiles)}
+    (
+      
+      <div key={file.path} className="card m-1 p-1">
+        {file.path}
+        {' '}
+        -
+        {file.size}
+        {' '}
+        bytes
+      </div>
+    )
+    }  
+    );
   
-  const rejectStyle = {
-    borderColor: '#ff1744',
-  };
-export function BulkDropzone(props) {
-    const baseStyle = {
-        flex: 1,
-        fontSize: props.fontSize,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: props.padding,
-        borderWidth: 2,
-        borderRadius: 2,
-        borderColor: '#eeeeee',
-        borderStyle: 'dashed',
-        backgroundColor: '#fafafa',
-        color: '#bdbdbd',
-        outline: 'none',
-        transition: 'border .24s ease-in-out',
-      };
-      const {
-        getRootProps,
-        getInputProps,
-        isDragActive,
-        isDragAccept,
-        isDragReject,
-        acceptedFiles,
-      } = useDropzone({accept:"csv/*"});
-  
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
 
   const style = useMemo(() => ({
     ...baseStyle,
@@ -56,16 +77,15 @@ export function BulkDropzone(props) {
     isDragReject,
     isDragAccept,
   ]);
+
   return (
-    <section className="container" >
-      <div {...getRootProps({ style })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+    <div {...getRootProps({ style })}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+      <div className="flex">
+        {acceptedFileItems}
       </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </section>
+      
+    </div>
   );
 }
--- src/app/views/leads/settings-form/BulkDragDrop.js
+++ src/app/views/leads/settings-form/BulkDragDrop.js
@@ -4,30 +4,39 @@ import { BulkDropzone}  from "../../../components/BulkDropzone";
 import { uploadFiles, selectMedia } from "../../../redux/actions/MediaActions";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper';
+import { useDispatch } from 'react-redux';
 
-const BulkDragDrop = () => {
+
+const BulkDragDrop = (props) => {
+// console.log(props, "bulkdrag")
   const [isUploading, setIsUploading] = useState(false);
   const [upload, setUpload] = useState(false);
-  
+  const [acceptedFileData, setAcceptedFileData] = useState("");
   const useStyles = makeStyles({
    textField:{
     width: '100%',
    }
   });
   const classes = useStyles();
-
+  const dispatch = useDispatch();
+  const updateAcceptedFileData = (updatedAcceptedFileData) => {
+    console.log("updateAcceptedFileDatffffa", updatedAcceptedFileData);
+    setAcceptedFileData(updatedAcceptedFileData);
+  }
+  console.log(acceptedFileData,"acceptedfileData")
   return (
     <div className="bulkContainer">
       <Grid container spacing={3} alignItems="center">
             <Grid item md={4}>
             <div className="ml-4 mr-2 mb-4 ">
-          <BulkDropzone
+            <BulkDropzone padding="0px 10px" fontSize="10px" updateAcceptedFileData={updateAcceptedFileData} uploadFiles={uploadFiles} hideZone={() => setUpload(false)} />
+
+          {/* <BulkDropzone
             padding="0px 10px"
             fontSize="10px"
-            // uploadFiles={uploadFiles}
+            uploadFiles={uploadFiles}
             // hideZone={() => setUpload(false)}
-          />
+          /> */}
         </div>
             </Grid>
             <Grid item md={4}>
@@ -42,7 +51,11 @@ const BulkDragDrop = () => {
             </Grid>
             <Grid item md={4}>
             <div className="bulkUpload  ml-2 mr-4 mb-4 ">
-          <Button fullWidth color="primary" variant="contained" type="submit">
+          <Button fullWidth color="primary" variant="contained" type="submit"  onClick={(e) => {
+            e.stopPropagation();
+            dispatch(uploadFiles(acceptedFileData));
+            // props.hideZone();
+          }}>
             {isUploading ? "Pending" : "Start Upload"}
           </Button>
         </div>
